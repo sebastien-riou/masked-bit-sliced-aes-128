d9b0e3ad6a541722ef9407d7abcc55440496613f
sebastien-riou <sriou@nimp.co.uk>
2017-04-20 23:47:35 +0200

    Update build_test.sh

--- build_test.sh
+++ build_test.sh
@@ -3,5 +3,5 @@ baseDir=.
 testDir=$baseDir/test
 sourceDir=$baseDir/source
 includeDir=$baseDir/include
-
-c++ -Ofast -std=c++11 $testDir/main.cpp $sourceDir/bitslice.c $sourceDir/secure_aes_pbs.c -I $includeDir -o masked-bit-sliced-aes-128 $*
+if [ -z ${CXX+x} ]; then CXX=c++; fi
+$CXX -Ofast -std=c++11 $testDir/main.cpp $sourceDir/bitslice.c $sourceDir/secure_aes_pbs.c -I $includeDir -o masked-bit-sliced-aes-128 $*
